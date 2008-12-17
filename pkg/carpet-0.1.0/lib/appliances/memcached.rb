37e5ff1896f420be1bb0ee07c732328e657c85ff
Matthias Marschall <mm@agileweboperations.com>
2008-12-17 15:19:40 +0100

    removed built stuff (pkg)

--- pkg/carpet-0.1.0/lib/appliances/memcached.rb
+++
@@ -1,8 +0,0 @@
-Capistrano::Configuration.instance(:must_exist).load do 
-  task :memcached do
-    assure :package, "SUNWmemcached"
-    svc.setprop("svc:/application/database/memcached", "memcached/options", "'(\"-u\" \"nobody\" \"-m\" \"#{fetch(:memcached_size, "1024")}\" \"-p\" \"#{fetch(:memcached_port, "11211")}\")'")
-    svc.refresh("svc:/application/database/memcached")
-    svc.enable("svc:/application/database/memcached")
-  end
-end
--- pkg/carpet-0.1.0/lib/appliances/mysql.rb
+++
@@ -1,7 +0,0 @@
-Capistrano::Configuration.instance(:must_exist).load do 
-  task :mysql do
-    assure :package, "SUNWmysql5"
-    svc.enable("svc:/application/database/mysql:version_50")
-    pfexec("/usr/mysql/bin/mysqladmin -u root password #{mysql_root_password} || exit 0") if exists?(:mysql_root_password)
-  end
-end
